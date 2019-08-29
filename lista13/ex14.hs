04def9bf7276f1d63070a31f26eee913008ca0d3
pacoca51 <phjfontes@gmail.com>
2019-08-29 19:04:50 -0300

    update ex14

--- lista13/ex14.hs
+++ lista13/ex14.hs
@@ -1,4 +1,13 @@
 pot :: Int -> Int -> Int
 pot m n
+    | n < 0 = error "Expoente negativo."
     | n == 0 = 1
-    | otherwise = m * pot m (n-1)
+    | n == 1 = m
+    | mod2 == 0 = potPar
+    | mod2 == 0 = potImpar
+    where
+        div2 = div n 2
+        mod2 = mod n 2
+        potPar = pot (m*m) (div2)
+        potImpar = m * potPar
+pot m n = m * pot m (n-1) 
