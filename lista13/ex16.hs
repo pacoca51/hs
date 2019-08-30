32c71d31fd467d2b3a5068322784ec02c9742fff
glitch-hash <54692677+glitch-hash@users.noreply.github.com>
2019-08-30 00:04:30 -0300

    changed to a working version

--- lista13/ex16.hs
+++ lista13/ex16.hs
@@ -1,1 +1,7 @@
-sublist x y = x == [x | x <- x, elem x y]
+sublista :: [Char] -> [Char] -> Bool
+sublista a b
+    | a == [] && b == [] = True
+    | a /= [] && b == [] = False
+    | a == [] && b /= [] = True
+    | (head a) == (head b) = (sublista (tail a) (tail b))
+    | otherwise = (sublista a (tail b))
