a046d4e8fb0e5af756a720b7606271b8b1fee8b7
glitch-hash <54692677+glitch-hash@users.noreply.github.com>
2019-08-29 23:05:08 -0300

    Update ex13.hs

--- lista13/ex13.hs
+++ lista13/ex13.hs
@@ -1,8 +1,7 @@
+aux :: (Int, Int) -> (Int, Int)
+aux (n, m) = (n+1,m)
+
 divMod' :: Int -> Int -> (Int, Int)
-divMod' x y
-    | x <= 0 = (0,0)
-    | y <= 0 = (0,0)
-    | otherwise = (divxy,modxy)
-    where
-        divxy = div x y
-        modxy = mod x y
+divMod' x y 
+    | x < y = (0, x)
+    | otherwise = aux (divMod' (x-y) y)
