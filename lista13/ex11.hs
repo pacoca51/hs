1780b1ab28b9117dca433339d06d541dae5cbdbd
glitch-hash <54692677+glitch-hash@users.noreply.github.com>
2019-08-29 23:10:28 -0300

    update ex11 to be "closer" to general recursion.

--- lista13/ex11.hs
+++ lista13/ex11.hs
@@ -1,6 +1,10 @@
-menor :: (Ord a) => [a] -> a
-menor [] = undefined
+f :: Int -> Int -> Int
+f x y
+    | x < y  = x
+    | x > y  = y
+    | x == y = x
+
+menor :: [Int] -> Int
+menor [] = 0
 menor [x] = x
-menor (x:xs)
-    | x <= (menor xs) = x
-    | otherwise = menor xs
+menor (x:xs) = f x (menor xs)
