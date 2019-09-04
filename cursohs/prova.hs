943f1c7e05d870de2ed9ee8ff38606a5a32a8ff0
seu nome <youremail@domain.com>
2019-09-04 19:50:30 -0300

    add prova

--- cursohs/prova.hs
+++ cursohs/prova.hs
@@ -24,3 +24,17 @@ filter' p (x:xs)
 
 zipWith' f (x:xs) (y:ys) = f x y : zipWith f xs ys
 zipWith' f _ _ = [ ]
+
+somaQ :: Int -> Int
+somaQ 0 = 0
+somaQ 1 = 1
+somaQ n = foldr1 (+) lista 
+        where
+            lista = map (^2) [1..n]
+
+somaP :: [Int] -> Int
+somaP [] = 0
+somaP n = foldr1 (+) lista 
+        where
+            lista = map (^2) positivos
+            positivos = filter (>0) n
